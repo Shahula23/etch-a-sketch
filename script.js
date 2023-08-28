c714919978e1bd69d87b60452061b2be3749e3a2
Saul Roland <saul@rolandfamily.co.uk>
2023-08-28 14:15:53 +0300

    Formatted and optimised code

--- script.js
+++ script.js
@@ -9,53 +9,56 @@ const normal = document.querySelector("#normal");
 createGrid(16);
 
 function createGrid(answer) {
-    document.querySelector("#gridContainer").innerHTML = "";
+    gridContainer.innerHTML = "";
     for(i = 0; i < answer * answer; i++) {
         let squareSize = 960 / answer;
         const gridItem = document.createElement("div");
         gridItem.classList.add("gridItem");
-        gridItem.style.height = `${squareSize}px`
-        gridItem.style.width = `${squareSize}px`
+        gridItem.style.height = `${squareSize}px`;
+        gridItem.style.width = `${squareSize}px`;
         gridContainer.appendChild(gridItem);
+        addListeners(gridItem);
+    };
+};
 
+function addListeners(gridItem) {
+    gridItem.addEventListener("mouseover", () => {
+        gridItem.classList.add("black");
+    });
+
+    eraser.addEventListener("click", () => {
+        gridItem.addEventListener("mouseover", () => {
+            gridItem.style.backgroundColor = "";
+            gridItem.classList.remove("black");
+            gridItem.classList.add("white");
+        });
+    });
+
+    clear.addEventListener("click", () => {
+        gridItem.style.backgroundColor = "";
+        gridItem.classList.add("white");
+        gridItem.addEventListener("mouseover", () => {
+            gridItem.classList.remove("white");
+            gridItem.classList.add("black");
+        });
+    });
+
+    rainbow.addEventListener("click", () => {
+        gridItem.addEventListener("mouseover", () => {
+            const randomR = Math.floor(Math.random() * 256);
+            const randomG = Math.floor(Math.random() * 256);
+            const randomB = Math.floor(Math.random() * 256);
+            gridItem.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+        });
+    });
+
+    normal.addEventListener("click", () => { 
         gridItem.addEventListener("mouseover", () => {
             gridItem.classList.add("black");
-        })
-
-        eraser.addEventListener("click", () => {
-            gridItem.addEventListener("mouseover", () => {
-                gridItem.style.backgroundColor = "";
-                gridItem.classList.remove("black");
-                gridItem.classList.add("white");
-            })
-        })
-
-        clear.addEventListener("click", () => {
+            gridItem.classList.remove("white");
             gridItem.style.backgroundColor = "";
-            gridItem.classList.add("white");
-            gridItem.addEventListener("mouseover", () => {
-                gridItem.classList.remove("white");
-                gridItem.classList.add("black");
-            })
-           })
-
-        rainbow.addEventListener("click", () => {
-            gridItem.addEventListener("mouseover", () => {
-                const randomR = Math.floor(Math.random() * 256);
-                const randomG = Math.floor(Math.random() * 256);
-                const randomB = Math.floor(Math.random() * 256);
-                gridItem.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
-            })
-        })
-
-        normal.addEventListener("click", () => { 
-            gridItem.addEventListener("mouseover", () => {
-                gridItem.classList.add("black");
-                gridItem.classList.remove("white");
-                gridItem.style.backgroundColor = "";
-            })
-        })
-    }
+        });
+    });
 }
 
 function newGrid() {
@@ -65,7 +68,7 @@ function newGrid() {
     } else if (answer <= 100 && answer >= 1) {
         createGrid(answer);
     } else if (answer > 100 || answer < 1 || typeof answer === "string") {
-        alert("Sorry, that is an incorrect input. Please try again.")
+        alert("Size should be between 1 and 100. Please try again.")
         newGrid();
     }
 }
